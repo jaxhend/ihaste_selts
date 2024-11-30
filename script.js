74c2b022ac461b46b9c05a391f6fa57631aa44d4
Hendrik Jaks <hendrikjaks@gmail.com>
2024-11-30 20:59:41 +0200

    global ja index valmis

--- script.js
+++
@@ -1,6 +0,0 @@
-// https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window
-// When clicked on a button, this function open a link in a new tab.
-
-function OpenInNewTab(url) {
-    window.open(url, '_blank').focus();
-  }
