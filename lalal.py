46006e3fee7b6039ce084a4e9e63b6d776ea7412
Kevin Markus Korrol <kevinmak@ut.ee>
2024-11-30 23:42:06 +0200

    final

---
+++ lalal.py
@@ -0,0 +1,47 @@
+<body>
+    <header>
+        <img src="https://raw.githubusercontent.com/jaxhend/ihaste_selts/refs/heads/main/logo.png" alt="Logo"
+            class="logo">
+        <div class="pealkiri">
+            <h1>Ihaste Seltsi üritused</h1>
+        </div>
+        <div class="menüü">
+            <a href="index.html" target="_self">Avaleht</a>
+            <a href="blogi.html" target="_self">Blogi</a>
+        </div>
+    </header>
+
+    <div class="banner-container">
+        <img src="https://raw.githubusercontent.com/jaxhend/ihaste_selts/refs/heads/main/banner.jpg" alt="banner" class="banner-image">
+        <div class="banner-text">
+            <h2>Tere tulemast Ihastesse</h2>
+        </div>
+    </div>
+
+    <div class="keskmine">
+        <div class="mingi_tekst">
+            <p>Varsti tulekul</p>
+            <div class="item">
+                <img src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/455652066_926909296144343_8852349450297899541_n.jpg?stp=dst-jpg_s600x600&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=sd37cWIsKScQ7kNvgHKpRH-&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=ALFKkvwbHZ0cE11EiDNTFK8&oh=00_AYDhb8Yv1zA9xDgpWB9dJ2VL7gftBA-2NxnJ9ogoQnr71Q&oe=674D650D" width="400px" height="300px">
+                <p>TEKST</p>
+            </div>
+            <div class="item">
+                <img src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/456154541_929766012525338_6465765102312493652_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=H2w9YhtMe_0Q7kNvgGflkYr&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=A7HvS0-ucxg--7S6Eh-IXMo&oh=00_AYAXxdJoBkxSgkSi6KYeQE3e6WG_571KA5VIgbKbgBUY0g&oe=674D3833" width="400px" height="300px">
+                <p>Tegemist on laheda asjaga tulge kindlasti kohale<br>lalalalal</p>
+            </div>
+            <div class="item">
+                <img src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/464200053_980857550749517_8017689139646971719_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=8dzpxJmBQysQ7kNvgEduCzo&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=AdjpvhMELrMcO33myn-SzeF&oh=00_AYDWhkoRBTcE197O9Aqn9NEiTPwf039rp1rxneJZrz0ZSw&oe=674D5CA1" width="400px" height="300px">
+                <p>TEKST</p>
+            </div>
+        </div>
+        <div class = "galerii">
+            <p>Jäädvustusi eelmistest üritustest</p>
+            <img class="mySlides" src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/464150628_983084127193526_3563111770376545082_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_ohc=1Q5CFroP1NUQ7kNvgFRdfg6&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=AXNlgVHU3aj52SXulqWsbff&oh=00_AYBMUqJGSGWx4fWHXSzzWnWA_zQgOB9WZES1nz70rJiTNA&oe=674C007B">
+            <img class="mySlides" src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/464327160_983084070526865_1417485714190260964_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Fu6p5fIC_jQQ7kNvgHaElcs&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=ATjf-boPFPRXDd1D-mNGdcZ&oh=00_AYDdiGo1H26vhE7rROJpA-QIvKIzLg2Wp2gcIH8tnGNL4A&oe=674C16AF">
+            <img class="mySlides" src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/465062790_983083987193540_3845344369617446213_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=8mLEDR4OGHgQ7kNvgGPtlJJ&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=AN1sFaNpmNLMdi1ZJFO7psi&oh=00_AYAxTi9gKNVXqkoPO5OpoTHTW1MA_HDvNHMZ__BM6JLCEg&oe=674C1D06">
+            <img class="mySlides" src="https://scontent-hel3-1.xx.fbcdn.net/v/t39.30808-6/464623559_982143367287602_1489475410453356936_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=j5wkPNwH6VEQ7kNvgEkRk5H&_nc_zt=23&_nc_ht=scontent-hel3-1.xx&_nc_gid=ALzLLJUeVglkhk9BfLOf65b&oh=00_AYD3dGZ4AuD9T5f8zXz4GNO7GzCweHrkGqWfWvKqumQ_Vg&oe=674C24D2">
+            <button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+            <button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+        </div>
+    </div>
+    <script src="galerii.js"></script>
