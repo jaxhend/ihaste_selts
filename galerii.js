a25ca643838702b20564dc61e14d19d9c3721b5c
Hendrik Jaks <hendrikjaks@gmail.com>
2024-12-01 23:21:31 +0200

    ok

--- galerii.js
+++ galerii.js
@@ -1,15 +1,15 @@
 var slideIndex = 1;
-showDivs(slideIndex); //näitab esimest pilti
+showDivs(slideIndex);  // näitab esimest pilti
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);//muudab muutuja slideIndex väärtust vastavalt sellele, kumba nuppu vajutati ja kuvab vastava pildi
+    showDivs(slideIndex += n);  // muudab muutuja slideIndex väärtust vastavalt sellele, kumba nuppu vajutati ja kuvab vastava pildi
 }
 
-function showDivs(n) {//funktsioon peidab kõik ülejäänud pildid ja kontrollib, et indeksid ei läheks piiridest välja
+function showDivs(n) {  // funktsioon peidab kõik ülejäänud pildid ja kontrollib, et indeksid ei läheks piiridest välja
     var i;
     var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}//kui indeks läheb suuremaks kui piltide arv, pannakse muutuja 1-ks ehk kuvatake esimene pilt
-    if (n < 1) {slideIndex = x.length} ;// kui indeks läheb negatiivseks pannakse muutuja väärtuseks piltide arv
+    if (n > x.length) {slideIndex = 1} // kui indeks läheb suuremaks kui piltide arv, pannakse muutuja 1-ks ehk kuvatake esimene pilt
+    if (n < 1) {slideIndex = x.length} ; // kui indeks läheb negatiivseks pannakse muutuja väärtuseks piltide arv
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
